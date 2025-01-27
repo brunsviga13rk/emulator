9ae345205203a4db0468c476db46dc2c68be91b8
servostar <sven.vogel123@web.de>
2025-01-27 12:35:27 +0000

    ops: merge origin/main into feat/loading-indicator

--- src/App.tsx
+++ src/App.tsx
@@ -5,55 +5,49 @@ import { StatusPanel } from './StatusPanel.tsx'
 function App() {
     return (
         <div className="flex flex-col h-full">
-            <header className="bg-gray-100 border-b-2">
-                <nav
-                    className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8"
-                    aria-label="Global"
+            <header className="flex flex-row w-full justify-around">
+                <div
+                    className="flex flex-row justify-start"
+                    id="header-inline-start"
                 >
-                    <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                className="h-12 w-auto"
-                                src="./brunsviga_icon.svg"
-                                alt=""
-                            />
-                        </a>
+                    <a href="https://github.com/brunsviga13rk">
+                        <img
+                            className="h-16 w-auto p-2 pl-4"
+                            src="./brains_of_steel.svg"
+                            alt=""
+                        />
+                    </a>
+                    <div className="flex flex-col" id="div-title">
+                        <span className="pl-2 pr-6" id="label-title">
+                            Brunsviga 13 RK
+                        </span>
+                        <span className="pl-2 pr-6" id="label-subtitle">
+                            Brains of steel
+                        </span>
                     </div>
-                    <div className="hidden lg:flex lg:gap-x-12">
-                        <a
-                            href="#"
-                            className="text-sm font-semibold leading-6 text-gray-900"
-                        >
-                            Brunsviga RK 13
-                        </a>
-                        <a
-                            href="#"
-                            className="text-sm font-semibold leading-6 text-gray-900 inline-flex items-center"
-                        >
-                            <Icon.FileEarmarkTextFill
-                                size={24}
-                                className="pr-2"
-                            />
-                            <span>Thesis</span>
-                        </a>
-                        <a
-                            href="https://github.com/brunsviga13rk/emulator"
-                            className="text-sm font-semibold leading-6 text-gray-900 inline-flex items-center"
-                        >
-                            <Icon.Github size={24} className="pr-2" />
-                            <span>Source</span>
-                        </a>
-                        <a
-                            href="#"
-                            className="text-sm font-semibold leading-6 text-gray-900  inline-flex items-center"
-                        >
-                            <Icon.PeopleFill size={24} className="pr-2" />
-                            Credits
-                        </a>
-                    </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end" />
-                </nav>
+                </div>
+                <div className="flex-grow"></div>
+                <div
+                    className="flex flex-row-reverse justify-end"
+                    id="header-inline-end"
+                >
+                    <a
+                        id="a-github"
+                        href="https://github.com/brunsviga13rk/emulator"
+                    >
+                        <Icon.Github size={32} />
+                    </a>
+                    <a href="#">About</a>
+                    <a href="#">Docs</a>
+                    <a href="https://github.com/brunsviga13rk/thesis">Paper</a>
+                    <a
+                        href={`https://github.com/brunsviga13rk/emulator/releases/tag/v${__APP_VERSION__}`}
+                        id="a-version"
+                    >
+                        {'v'}
+                        {__APP_VERSION__}
+                    </a>
+                </div>
             </header>
             <Renderer />
             <StatusPanel />
--- src/StatusPanel.tsx
+++ src/StatusPanel.tsx
@@ -1,14 +1,10 @@
 export function StatusPanel() {
     return (
-        <div id="status-panel" className="p-4 border-t-2">
+        <div id="status-panel" className="p-4">
             <span
                 id="input-action-recommendations"
                 className="absolute left-0 bottom-0 m-1"
             ></span>
-            <span className="absolute right-0 bottom-0 m-1 mr-4 text-zinc-600">
-                {'v'}
-                {__APP_VERSION__}
-            </span>
         </div>
     )
 }
