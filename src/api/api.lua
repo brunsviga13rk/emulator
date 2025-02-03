727d28e8a6aeb1af5d7a140391d8a5c3ff2166b5
servostar <sven.vogel123@web.de>
2025-02-03 12:00:36 +0100

    feat: wrap typescript API in lua

---
+++ src/api/api.lua
@@ -0,0 +1,64 @@
+
+--- Get Typescript API abstraction.
+---@type any
+---@diagnostic disable-next-line: undefined-global
+local ts_api = ts_api_get_1_3_0()
+
+local M = {}
+
+--- Reset all registers of the machine to a zero value.
+--- Execution time depends on the animation time of task.
+function M.reset()
+    ts_api.reset():await()
+end
+
+--- Set the counter register to zero.
+--- Execution time depends on the animation time of task.
+function M.reset_counter(value)
+    ts_api.zero_counter_register():await()
+end
+
+--- Set the input register to zero.
+--- Execution time depends on the animation time of task.
+function M.reset_input(value)
+    ts_api.zero_input_register():await()
+end
+
+--- Set the result register to zero.
+--- Execution time depends on the animation time of task.
+function M.reset_result(value)
+    ts_api.zero_result_register():await()
+end
+
+--- Add the value of the input register onto the result register.
+--- Execution time depends on the animation time of task.
+function M.add()
+    ts_api.add():await()
+end
+
+--- Subtract the value of the input register onto the result register.
+--- Execution time depends on the animation time of task.
+function M.subtract()
+    ts_api.subtract():await()
+end
+
+--- Set the given value to the input register.
+--- Execution time depends on the animation time of task.
+---@param value integer
+function M.load(value)
+    ts_api.load(value):await()
+end
+
+--- Shift the result register by one digit to the left.
+--- Execution time depends on the animation time of task.
+function M.shift_left(value)
+    ts_api.shift_left():await()
+end
+
+--- Shift the result register by one digit to the right.
+--- Execution time depends on the animation time of task.
+function M.shift_right(value)
+    ts_api.shift_right():await()
+end
+
+return M
--- src/api/lua.tsx
+++ src/api/lua.tsx
@@ -1,23 +1,71 @@
 import { LuaFactory } from 'wasmoon'
 import { Brunsviga13rk } from '../model/brunsviga13rk'
+import luaAPI from './api.lua?raw'
+
+const TS_API_NAME = `ts_api_get_${__APP_VERSION__}`.replace(/\./g, '_')
 
 // Initialize a new lua environment factory
 // You can pass the wasm location as the first argument, useful if you are using
 // wasmoon on a web environment and want to host the file by yourself.
 const factory = new LuaFactory()
+
+// Mount accessible Brunsviga 13 RK Lua API as module.
+factory.mountFile('./api.lua', luaAPI)
+
 // Create a standalone lua environment from the factory.
-const lua = await factory.createEngine()
+const lua = await factory.createEngine({ injectObjects: true })
 
 // Wrap singleton API of Brunsviga 13 RK to Lua
 // ============================================================================
 //
 
-lua.global.set('set', (v: number) => Brunsviga13rk.getInstance().setInput(v))
-lua.global.set('add', () => Brunsviga13rk.getInstance().add())
-lua.global.set('sub', () => Brunsviga13rk.getInstance().subtract())
-lua.global.set('shr', () => Brunsviga13rk.getInstance().shiftLeft())
-lua.global.set('shl', () => Brunsviga13rk.getInstance().shiftRight())
-lua.global.set('clr', () => Brunsviga13rk.getInstance().clearRegisters())
+/**
+ * Wrapper API for static Brunsviga Typescript API tied in with animaiton.
+ * The purpose of this class is to hide all functions and accessors of the TS
+ * API undesired in the Lua API. All API calls are asynchronous and must be
+ * awaited in order to preserve the correct state of the machine.
+ */
+class Brunsviga13rkLuaAPI {
+    private _instance = Brunsviga13rk.getInstance()
+
+    public async load(value: number) {
+        await this._instance.setInput(value)
+    }
+
+    public async add() {
+        await this._instance.add()
+    }
+
+    public async subtract() {
+        await this._instance.subtract()
+    }
+
+    public async reset() {
+        await this._instance.clearRegisters()
+    }
+
+    public async zero_counter_register() {
+        await this._instance.clearCounterRegister()
+    }
+
+    public async zero_input_register() {
+        await this._instance.clearInputRegister()
+    }
+
+    public async zero_result_register() {
+        await this._instance.clearOutputRegister()
+    }
+
+    public async shift_left() {
+        await this._instance.shiftLeft()
+    }
+
+    public async shift_right() {
+        await this._instance.shiftRight()
+    }
+}
+
+lua.global.set(TS_API_NAME, () => new Brunsviga13rkLuaAPI())
 
 export function execute(script: string) {
     new Promise(() => lua.doString(script))
