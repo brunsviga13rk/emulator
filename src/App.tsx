d69d718ee693e316c012c709f30769d9a1ea305e
servostar <72654954+Servostar@users.noreply.github.com>
2025-02-09 21:54:51 +0100

    feat: add embed page route (#96)
    build: add react-router package

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import Renderer from './render/Renderer.tsx'
 import { Header } from './Header.tsx'
 import { Editor as Solver } from './solver/Editor.tsx'
 import { Editor as Coder } from './api/Editor.tsx'
-import {
-    Box,
-    CircularProgress,
-    Grid2,
-    Paper,
-    Stack,
-    Tab,
-    Tabs,
-} from '@mui/material'
+import { Box, Grid2, Paper, Stack, Tab, Tabs } from '@mui/material'
 import Dashboard from './Dashboard.tsx'
 import { useState } from 'react'
 import CalculateIcon from '@mui/icons-material/Calculate'
 import CodeIcon from '@mui/icons-material/Code'
+import { LoadingIndicator } from './LoadingIndicator.tsx'
 
 interface TabPanelProps {
     children?: React.ReactNode
@@ -124,15 +117,7 @@ function App() {
                     </Box>
                 </Grid2>
             </Grid2>
-            <div
-                id="div-loading-indicator"
-                className="flex h-full w-full absolute bg-white bg-opacity-100 z-100"
-            >
-                <div className="m-auto flex-col justify-center">
-                    <CircularProgress color="inherit" size={60} />
-                    <p className="mt-8">Loading...</p>
-                </div>
-            </div>
+            <LoadingIndicator />
         </Stack>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@phosphor-icons/web/regular/style.css'
 import '@phosphor-icons/web/bold/style.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { LoadingIndicator } from './LoadingIndicator.tsx'
+import Renderer from './render/Renderer.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route
+                path="/embed"
+                element={
+                    <>
+                        <Renderer />
+                        <LoadingIndicator />
+                    </>
+                }
+            />
+        </Routes>
+    </BrowserRouter>
 )
