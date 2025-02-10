b66d287fe9d04f1d726c2ab5e7dc7085d884684c
servostar <sven.vogel123@web.de>
2025-02-10 14:49:59 +0100

    feat: throw error when encountering errors in parsing

--- src/solver/solve.tsx
+++ src/solver/solve.tsx
@@ -1,5 +1,12 @@
 import { Brunsviga13rk } from '../model/brunsviga13rk'
 
+/**
+ * Convert a string containing a calculation to an array of tokens.
+ * Whitespace will be discarded.
+ *
+ * @param text
+ * @returns An array of tokens.
+ */
 function tokenize(text: string): Token[] {
     return text
         .split(/\s+|([+\-*/(),])/)
@@ -7,23 +14,38 @@ function tokenize(text: string): Token[] {
         .map((text) => new Token(text))
 }
 
+/**
+ * A token may either be a number of the format `[0-9]+`
+ * or an operator of the following symbols: +,-,*,/.
+ */
 export enum TokenKind {
     Number,
     Operator,
     Function,
 }
 
+/**
+ * Token representing a logical atomic brick of a calculation.
+ * A token is composed of a kind and an optional value.
+ * This value maybe a string or a number.
+ */
 export class Token {
     kind: TokenKind
     value: string | number
 
     constructor(text: string) {
+        // Check if text is a number.
         if (text.match(/[0-9]+/)) {
             this.kind = TokenKind.Number
             this.value = Number.parseInt(text)
+
+            // If we do not have a number the token may be an operator.
         } else if (text.match(/[+\-*/(),]/)) {
             this.kind = TokenKind.Operator
             this.value = text
+
+            // In case neither a number nor a operator are supplied
+            // this token must be a function.
         } else {
             this.kind = TokenKind.Function
             this.value = text
@@ -42,6 +64,12 @@ export class Token {
         return this.kind == TokenKind.Function
     }
 
+    /**
+     * Operators with higher precedence will bind operators to them before
+     * operators with lower precedences.
+     *
+     * @returns The precedence of the operator.
+     */
     getPrecedence(): number {
         switch (this.value as string) {
             case '+':
@@ -67,15 +95,39 @@ export class Token {
     }
 }
 
+/**
+ * Convert an array of tokens in infix notation to postfix notation.
+ * Infix notation is the "natural" sequence we express calculations by
+ * sourrounding the operator by its operands. By postfix notation the operator
+ * follows the operands. This implementation makes use the textbook version
+ * of the shunting yard algorithm for conversion.
+ *
+ * Example:
+ * ```
+ * infix: 2 + 4 * 89
+ * postfix: 2 4 89 * +
+ * ```
+ *
+ * @throws Error
+ * @param tokens
+ * @returns
+ */
 function infix2postfix(tokens: Token[]): Token[] {
+    // Stores the postfix output.
     const output: Token[] = []
+    // Temporary stack storing tokens not yet pushed to the output.
     const stack: Token[] = []
 
     tokens.forEach((token) => {
+        // Push numbers, functions and '(' directly to the output.
         if (token.isNumber()) {
             output.push(token)
-        } else if (token.isFunction()) {
+        } else if (token.isFunction() || token.value == '(') {
             stack.push(token)
+
+            // TODO: implment functions.
+            if (token.isFunction())
+                throw new Error(`Function not supported: ${token.value}`)
         } else if (token.value == ',') {
             while (stack.length) {
                 const top = stack.pop()!
@@ -87,22 +139,23 @@ function infix2postfix(tokens: Token[]): Token[] {
                     break
                 }
             }
-        } else if (token.value == '(') {
-            stack.push(token)
         } else if (token.value == ')') {
+            // Pop all non-left parenthesis from the stack.
             while (stack.length && stack.at(-1)!.value != '(') {
                 output.push(stack.pop()!)
             }
 
-            stack.pop()
+            // There must be a left parenthesis ontop of the stack.
+            if (!stack.pop()) {
+                throw new Error('Mismatched parenthesis in calculation')
+            }
 
-            const top = stack.at(-1)
-            if (top) {
-                if (top.isFunction()) {
-                    output.push(stack.pop()!)
-                }
+            if (stack.at(-1)?.isFunction()) {
+                output.push(stack.pop()!)
             }
         } else if (token.isOperator()) {
+            // Move all operators with higher precedence from the stack onto
+            // the output.
             while (stack.length) {
                 const top = stack.at(-1)!
 
@@ -121,11 +174,15 @@ function infix2postfix(tokens: Token[]): Token[] {
         }
     })
 
+    // Put all tokens remaing on the stack onto the output.
     stack.reverse().forEach((token) => output.push(token))
 
     return output
 }
 
+/**
+ * Valid operations the Brunsviga can execute.
+ */
 export enum Opcode {
     Zero, // Clear output register.
     Add, // Add input register onto output register.
@@ -221,14 +278,31 @@ export class Instruction {
     }
 }
 
+/**
+ * Compile an array of Tokens in postfix notation to instructions for the 3
+ * register machine.
+ *
+ * @throws Error
+ * @param tokens
+ * @returns
+ */
 function compile(tokens: Token[]): Instruction[] {
     const stack: number[] = []
     const prog: Instruction[] = []
 
+    // Keep track of the state of both the input and result register.
     let inputRegisterValue = 0
     let resultRegisterValue = 0
 
-    tokens.forEach((token) => {
+    for (const token of tokens) {
+        // Check for missmatched parenthesis
+        if (
+            typeof token.value === 'string' &&
+            ['(', ')'].includes(token.value)
+        ) {
+            throw new Error('Mismatched parenthesis in calculation')
+        }
+
         // Operation loading a value into the result sprocket accounting for the
         // sign of the value.
         const load = (value: number) => {
@@ -254,8 +328,12 @@ function compile(tokens: Token[]): Instruction[] {
             }
 
             // Get operands from stack.
-            const op0 = stack.pop()!
-            const op1 = stack.pop()!
+            const op0 = stack.pop()
+            const op1 = stack.pop()
+
+            if (!op0) throw new Error('Missimg another operator')
+
+            if (!op1) throw new Error('Missimg another operator')
 
             let result = 0
 
@@ -347,7 +425,7 @@ function compile(tokens: Token[]): Instruction[] {
 
             stack.push(result)
         }
-    })
+    }
 
     return prog
 }
