dcd3e62a1341d6cbc3da4267c1594a43f0b35699
servostar <72654954+Servostar@users.noreply.github.com>
2025-02-10 12:45:02 +0100

    feat: add text logo to loading screen (#107)

--- src/LoadingIndicator.tsx
+++ src/LoadingIndicator.tsx
@@ -1,4 +1,5 @@
 import CircularProgress from '@mui/material/CircularProgress'
+import { TextLogo } from './TextLogo'
 
 export function LoadingIndicator() {
     return (
@@ -6,9 +7,12 @@ export function LoadingIndicator() {
             id="div-loading-indicator"
             className="flex h-full w-full absolute bg-white bg-opacity-100 z-100"
         >
-            <div className="m-auto flex-col justify-center">
+            <div className="m-auto flex-col text-center">
                 <CircularProgress color="inherit" size={60} />
-                <p className="mt-8">Loading...</p>
+                <p style={{ marginTop: '2.5rem' }}>
+                    <TextLogo width="12rem" />
+                </p>
+                <p style={{ marginTop: '1rem' }}>Loading assets...</p>
             </div>
         </div>
     )
