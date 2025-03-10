6957d7c8e537f38e138d232e45737786d44b1948
servostar <sven.vogel123@web.de>
2025-03-10 16:53:31 +0100

    fix: switch to drawer menu when header  is too narrow

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,14 @@ import {
     Box,
     Button,
     Chip,
+    Divider,
+    Drawer,
     IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
     Stack,
     Toolbar,
     Typography,
@@ -17,9 +24,91 @@ import ContrastIcon from '@mui/icons-material/Contrast'
 import { BrunsvigaLogo } from './BrunsvigaLogo'
 import { environmentUniforms } from './render/environment'
 import { isDarkMode } from './utils'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
+import DescriptionIcon from '@mui/icons-material/Description'
+import InfoIcon from '@mui/icons-material/Info'
+import NewspaperIcon from '@mui/icons-material/Newspaper'
 
 export function Header() {
     const { mode, setMode } = useColorScheme()
+
+    const [open, setOpen] = useState(false)
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen)
+    }
+
+    const drawerList = (
+        <Box
+            sx={{ width: '16rem' }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+        >
+            <Stack direction="row" margin="1rem">
+                <BrunsvigaLogo />
+                <Stack spacing={0} sx={{ marginRight: 4, marginLeft: 1 }}>
+                    <TextLogo width="8rem" />
+                    <Typography variant="body2" component="div">
+                        BRAINS OF STEEL
+                    </Typography>
+                </Stack>
+            </Stack>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        href="https://brunsviga13rk.github.io/docs"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <ListItemIcon>
+                            <DescriptionIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Docs" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        href={`https://github.com/brunsviga13rk/thesis/releases/download/v${__APP_VERSION__}/44124_emulation-of-the-brunsviga-13-rk.pdf`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <ListItemIcon>
+                            <NewspaperIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Paper" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        color="inherit"
+                        href="https://github.com/brunsviga13rk/emulator"
+                        target="_blank"
+                    >
+                        <ListItemIcon>
+                            <GitHubIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Source" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton
+                        href="https://github.com/brunsviga13rk"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <ListItemIcon>
+                            <InfoIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="About" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    )
 
     return (
         <AppBar
@@ -45,56 +134,75 @@ export function Header() {
                     variant="outlined"
                 />
                 <Box sx={{ flexGrow: 1 }}></Box>
-                <IconButton
-                    onClick={() => {
-                        const nextMode =
-                            mode == 'dark'
-                                ? 'light'
-                                : mode == 'light'
-                                  ? 'system'
-                                  : 'dark'
-                        setMode(nextMode)
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Box>
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <IconButton
+                        size="large"
+                        onClick={() => {
+                            const nextMode =
+                                mode == 'dark'
+                                    ? 'light'
+                                    : mode == 'light'
+                                      ? 'system'
+                                      : 'dark'
+                            setMode(nextMode)
 
-                        environmentUniforms.darkMode.value =
-                            isDarkMode(nextMode)
-                    }}
-                >
-                    {mode == 'system' && <ContrastIcon />}
-                    {mode == 'dark' && <DarkModeIcon />}
-                    {mode == 'light' && <LightModeIcon />}
-                </IconButton>
-                <Button
-                    href="https://brunsviga13rk.github.io/docs"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="inherit"
-                >
-                    Docs
-                </Button>
-                <Button
-                    href={`https://github.com/brunsviga13rk/thesis/releases/download/v${__APP_VERSION__}/44124_emulation-of-the-brunsviga-13-rk.pdf`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="inherit"
-                >
-                    Paper
-                </Button>
-                <Button
-                    href="https://github.com/brunsviga13rk"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="inherit"
-                >
-                    About
-                </Button>
-                <IconButton
-                    href="https://github.com/brunsviga13rk/emulator"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <GitHubIcon />
-                </IconButton>
+                            environmentUniforms.darkMode.value =
+                                isDarkMode(nextMode)
+                        }}
+                    >
+                        {mode == 'system' && <ContrastIcon />}
+                        {mode == 'dark' && <DarkModeIcon />}
+                        {mode == 'light' && <LightModeIcon />}
+                    </IconButton>
+                    <Button
+                        href="https://brunsviga13rk.github.io/docs"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        color="inherit"
+                    >
+                        Docs
+                    </Button>
+                    <Button
+                        href={`https://github.com/brunsviga13rk/thesis/releases/download/v${__APP_VERSION__}/44124_emulation-of-the-brunsviga-13-rk.pdf`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        color="inherit"
+                    >
+                        Paper
+                    </Button>
+                    <Button
+                        href="https://github.com/brunsviga13rk"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        color="inherit"
+                    >
+                        About
+                    </Button>
+                    <IconButton
+                        size="large"
+                        color="inherit"
+                        href="https://github.com/brunsviga13rk/emulator"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <GitHubIcon />
+                    </IconButton>
+                </Box>
             </Toolbar>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {drawerList}
+            </Drawer>
         </AppBar>
     )
 }
