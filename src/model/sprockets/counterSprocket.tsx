f883a2b33cb64155c77af882bc5f014dd98493bd
servostar <72654954+Servostar@users.noreply.github.com>
2025-05-06 23:33:45 +0200

    feat: add missing animations (#177)
    feat: add input register reset indicator
    feat: add animation for counter reset handle
    feat: add animation for counter shift indicator
    feat: add counter start indicator
    feat: implement animation for negative counter values
    
    * chore: add updated model

--- src/model/sprockets/counterSprocket.tsx
+++ src/model/sprockets/counterSprocket.tsx
@@ -1,17 +1,160 @@
-import { Group, Object3DEventMap } from 'three'
+import { Group, Object3D, Object3DEventMap } from 'three'
 import { EventHandler } from '../events'
-import { SprocketWheel } from './sprocketWheel'
+import { SprocketWheel, SprocketWheelEventType } from './sprocketWheel'
 import { Brunsviga13rk } from '../brunsviga13rk'
 import { OperationHandleEventType } from '../handles/operationHandle'
 import { HandleEventType } from '../handles/handle'
+import {
+    AnimationScalarState,
+    AnimationScalarStateEventType,
+    CubicEaseInOutInterpolation,
+} from '../animation'
+import { SledEventType } from '../sled'
 
 export class CounterSprocket extends SprocketWheel {
+    protected counterIndicator: Object3D<Object3DEventMap>
+    private counterIndicatorAnimation: AnimationScalarState
+    private counterOffset: number
+
+    private signPanel: Object3D<Object3DEventMap>
+    private signPanelAnimation: AnimationScalarState
+    private signSet: boolean
+
+    private startIndicator: Object3D<Object3DEventMap>
+    private startIndicatorAnimation: AnimationScalarState
+    private stateReset: boolean
+
+    private resetIndicator: Object3D<Object3DEventMap>
+    private resetIndicatorAnimation: AnimationScalarState
+
     public constructor(scene: Group<Object3DEventMap>) {
         super(scene, 'counter_sprocket_wheel', 8, 0, 2 * Math.PI, 10)
+
+        this.signSet = false
+        this.signPanel = scene.getObjectByName('counter_panel')!
+        this.signPanelAnimation = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.1
+        )
+        this.signPanelAnimation.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.signPanel.position.z += delta as number
+            })
+        )
+
+        this.stateReset = false
+        this.startIndicator = scene.getObjectByName('counter_start_indicator')!
+        this.startIndicatorAnimation = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.1
+        )
+        this.startIndicatorAnimation.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.startIndicator.rotation.y += delta as number
+            })
+        )
+
+        this.counterOffset = 0
+        this.counterIndicator = scene.getObjectByName(
+            'counter_shift_indicator'
+        )!
+        this.counterIndicatorAnimation = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.1
+        )
+        this.counterIndicatorAnimation.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.counterIndicator.position.z += delta as number
+            })
+        )
+
+        this.resetIndicator = scene.getObjectByName('counter_delete_indicator')!
+        this.resetIndicatorAnimation = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.25
+        )
+        this.resetIndicatorAnimation.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.resetIndicator.rotation.y += delta as number
+            })
+        )
+    }
+
+    public reset() {
+        this.resetIndicatorAnimation.targetState = -Math.abs(
+            Math.PI * 2.0 - this.resetIndicatorAnimation.currentState
+        )
+        this.startIndicatorAnimation.targetState = 0
+
+        this.signPanelAnimation.targetState = 0
+
+        this.signSet = false
+
+        this.stateReset = true
+        super.reset()
+        this.stateReset = false
+    }
+
+    public perform(delta: number): void {
+        this.resetIndicatorAnimation.advance(delta)
+        this.counterIndicatorAnimation.advance(delta)
+        this.startIndicatorAnimation.advance(delta)
+        this.signPanelAnimation.advance(delta)
+        super.perform(delta)
     }
 
     public registerActionEvents() {
         this.registerDeleteHandleEvents()
+        this.registerIndicatorEvents()
+
+        this.getEmitter().subscribe(
+            SprocketWheelEventType.Decrement,
+            new EventHandler(() => {
+                if (!this.signSet && !this.stateReset) {
+                    this.signPanelAnimation.targetState = -0.0027
+                }
+            })
+        )
+
+        this.getEmitter().subscribe(
+            SprocketWheelEventType.Change,
+            new EventHandler(() => {
+                if (!this.stateReset) {
+                    this.startIndicatorAnimation.targetState = Math.PI
+                    this.signSet = true
+                }
+            })
+        )
+    }
+
+    private registerIndicatorEvents() {
+        const emitter = Brunsviga13rk.getInstance().sled.getEmitter()
+
+        emitter.subscribe(
+            SledEventType.ShiftLeft,
+            new EventHandler(() => {
+                this.counterOffset--
+                this.counterIndicatorAnimation.targetState =
+                    this.counterOffset * 0.00925
+            })
+        )
+
+        emitter.subscribe(
+            SledEventType.ShiftRight,
+            new EventHandler(() => {
+                this.counterOffset++
+                this.counterIndicatorAnimation.targetState =
+                    this.counterOffset * 0.00925
+            })
+        )
     }
 
     private registerDeleteHandleEvents() {
--- src/model/sprockets/inputSprocket.tsx
+++ src/model/sprockets/inputSprocket.tsx
@@ -1,4 +1,4 @@
-import { Group, Object3DEventMap } from 'three'
+import { Group, Object3D, Object3DEventMap } from 'three'
 import { EventHandler } from '../events'
 import { SprocketWheel } from './sprocketWheel'
 import { Brunsviga13rk } from '../brunsviga13rk'
@@ -8,6 +8,11 @@ import {
     InputWheelIncrementEvent,
 } from './inputWheel'
 import { HandleEventType } from '../handles/handle'
+import {
+    AnimationScalarState,
+    AnimationScalarStateEventType,
+    CubicEaseInOutInterpolation,
+} from '../animation'
 
 /**
  * Maximum value the input sprocket supports.
@@ -15,8 +20,37 @@ import { HandleEventType } from '../handles/handle'
 export const MAX_INPUT_SPROCKET_VALUE = 9999999999
 
 export class InputSprocket extends SprocketWheel {
+    private resetIndicator: Object3D<Object3DEventMap>
+    private resetIndicatorAnimation: AnimationScalarState
+
     public constructor(scene: Group<Object3DEventMap>) {
         super(scene, 'input_sprocket_wheel', 10, 0, 3.925, 10)
+
+        this.resetIndicator = scene.getObjectByName('input_reset_indicator')!
+        this.resetIndicatorAnimation = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.25
+        )
+        this.resetIndicatorAnimation.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.resetIndicator.rotation.y += delta as number
+            })
+        )
+    }
+
+    public reset() {
+        this.resetIndicatorAnimation.targetState = -Math.abs(
+            Math.PI * 2.0 - this.resetIndicatorAnimation.currentState
+        )
+
+        super.reset()
+    }
+
+    public perform(delta: number): void {
+        this.resetIndicatorAnimation.advance(delta)
+        super.perform(delta)
     }
 
     public registerActionEvents() {
