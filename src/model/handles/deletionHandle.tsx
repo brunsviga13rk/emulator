d5ec9e8ff4df032feaa68f9cb50f6061929b3973
servostar <72654954+Servostar@users.noreply.github.com>
2025-05-07 21:23:40 +0200

    feat: add gear animation (#181)

---
+++ src/model/handles/deletionHandle.tsx
@@ -0,0 +1,52 @@
+import { Group, Object3D, Object3DEventMap } from 'three'
+import { EventHandler } from '../events'
+import {
+    AnimationScalarState,
+    AnimationScalarStateEventType,
+    CubicEaseInOutInterpolation,
+} from '../animation'
+import { Handle, HandleEventType } from './handle'
+
+export class DeletionHandle extends Handle {
+    protected gearMesh: Object3D<Object3DEventMap>
+    protected gearAnimationState: AnimationScalarState
+
+    public constructor(scene: Group<Object3DEventMap>) {
+        super(scene, 'deletion', 0, 1.75)
+
+        this.gearMesh = scene.getObjectByName('gear')!
+        this.gearAnimationState = new AnimationScalarState(
+            0,
+            CubicEaseInOutInterpolation,
+            0.5
+        )
+        this.gearAnimationState.getEmitter().subscribe(
+            AnimationScalarStateEventType.StateChanged,
+            new EventHandler((delta) => {
+                this.gearMesh.rotation.y += delta as number
+            })
+        )
+    }
+
+    public registerEventSubscribtions() {
+        super.getEmitter().subscribe(
+            HandleEventType.PullDown,
+            new EventHandler(() => {
+                this.gearAnimationState.targetState = 1.75
+            })
+        )
+
+        super.getEmitter().subscribe(
+            HandleEventType.PullDownDone,
+            new EventHandler(() => {
+                this.gearAnimationState.targetState = 0
+            })
+        )
+    }
+
+    perform(delta: number): void {
+        this.gearAnimationState.advance(delta)
+
+        super.perform(delta)
+    }
+}
