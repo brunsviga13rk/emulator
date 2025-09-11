7d15656d99fb224fe5a30d98eb0cbf51555c0095
servostar <sven.vogel123@web.de>
2025-09-12 00:19:20 +0200

    feat: add baseplane with baked shadow
    Signed-off-by: servostar <sven.vogel123@web.de>

--- src/baseplane.tsx
+++ src/baseplane.tsx
@@ -1,18 +1,5 @@
-import vertexShader from './shader/concentricGradientAlpha/vertexShader.glsl?raw'
-import fragmentShader from './shader/concentricGradientAlpha/fragmentShader.glsl?raw'
-import {
-    Mesh,
-    NormalBlending,
-    Object3D,
-    PlaneGeometry,
-    ShaderMaterial,
-} from 'three'
-import { environmentUniforms } from './render/environment'
-
-/**
- * Radius of the base plane.
- */
-const BASE_PLANE_RADIUS = 0.5
+import { Scene } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Create a base plane mesh with a white gradient decreasing in opaqueness
@@ -20,23 +7,9 @@ const BASE_PLANE_RADIUS = 0.5
  *
  * @returns An Object3D that is the base plane.
  */
-export function createBaseplane(): Object3D {
-    const plane = new PlaneGeometry(BASE_PLANE_RADIUS, BASE_PLANE_RADIUS, 1, 1)
-    const mesh = new Mesh(
-        plane,
-        new ShaderMaterial({
-            vertexShader: vertexShader,
-            fragmentShader: fragmentShader,
-            blending: NormalBlending,
-            transparent: true,
-            uniforms: environmentUniforms,
-        })
-    )
-
-    // Rotate base plane so that its orientation matches that of a logical "ground".
-    mesh.rotation.x = -Math.PI * 0.5
-    // Move down one unit as the Brunsviga mesh will extend downwards by one.
-    mesh.position.y = -0.080084
-
-    return mesh
+export function createBaseplane(scene: Scene) {
+    const loader = new GLTFLoader()
+    loader.load(`${__APP_BASE_PATH__}/baseplane.glb`, (gltf) => {
+        scene.add(gltf.scene)
+    })
 }
